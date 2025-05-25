3a8723b80abe172913bf32ef556db64e093537e7
DavinMetoti <DavinMetotii@gmail.com>
2025-05-25 23:58:13 +0700

    update git ignore dan env

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: '*', // atau domain Angular kamu
+    methods: ['GET', 'POST']
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('send-message', (data) => {
+    console.log('Received message:', data);
+    // broadcast ke semua client
+    io.emit('receive-message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Socket.IO server running on http://localhost:${PORT}`);
+});
